97885e32a5afa48750f36a16e62000eb4ac9b7eb
rmillard3 <161741161+rmillard3@users.noreply.github.com>
2024-03-28 14:25:20 -0500

    Python Script File: project check-in 2

---
+++ pythonscriptfile.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+
+
+class Book:
+
+
+	def __init__ (self):
+		# this will hold the variables
+
+
+	def program_window(self):
+		# the window where everything would be taking place will be built inside this function
+		# this will eventually be integrate into the next function so that everything takes place in the correct place
+
+	def take_questionnaire(self):
+		# this is where all of the questions will be held
+		# depending on what users answers, they will be taken on a different path of questions, and this is where all of those would be held
+
+	def book_recommendation(self):
+		# the dictionary containing all of the possible book options will be inside this function
+		# based on the answers that the user chose, this is where the book will be chosen from the list of books in the dictionary
+
+	def display_book(self):
+		# this function is where the code will be written to actually display the book info - whether that be written text or a picture of the cover
+
+
+
+def main():
+
+if __name__ == "__main__":
+    main()
